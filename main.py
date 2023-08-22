22d49835000babcf217c6ca16a83890721840a0d
sugab <sugabida@gmail.com>
2023-08-22 16:27:57 +0700

    feat: change to minimal feat for program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from modules.license_plate import LicensePlateDetector
 
 
@@ -8,30 +9,58 @@ def main():
     parser.add_argument("image_path", type=str, help="Path to the input image")
     parser.add_argument("-a", "--auto_canny", type=int, default=0,
                         help="Use auto Canny edge detection (1) or not (0)")
-    parser.add_argument("-b", "--binary", type=int, default=0,
-                        help="Use ROI Thresholding (binarization) on OCR process (1) or not (0)")
-    parser.add_argument("-d", "--debug", type=int, default=0,
-                        help="Show debug image per process (1) or not (0)")
     args = parser.parse_args()
 
     lp = LicensePlateDetector(
-        args.image_path, auto_canny=args.auto_canny, binary=args.binary, debug=args.debug)
+        args.image_path, auto_canny=args.auto_canny)
 
-    ok, roi, plate_contour = lp.find_plate()
+    sorted_contours = lp.find_contour()
+
+    # find plate method 1 by corner
+    ok, _ = lp.find_plate_by_corner(sorted_contours)
+    print("Method 1: by 4 Corner")
+
     if not ok:
+        print("[FAILED]")
         print("Licensed Plate Contour Not Found")
-        return
-
-    ok, license_plate_text = lp.plate_ocr(roi)
-
-    if ok:
-        print("License Plate Detected:", license_plate_text)
     else:
-        print("Error or Plate Not Detected")
+        print("[SUCCESS]")
+        print("Licensed Plate Contour Found")
+    print()
 
-    lp.draw_result(plate_contour, license_plate_text)
-    print("Result image saved as result.jpg")
+    # find plate method 2 by aspect ratio and area
+    ok, _ = lp.find_plate_by_ratio(sorted_contours)
+    print("Method 2: by Aspect Ratio and Area")
+
+    if not ok:
+        print("[FAILED]")
+        print("Licensed Plate Contour Not Found")
+    else:
+        print("[SUCCESS]")
+        print("Licensed Plate Contour Found")
+    print()
+
+
+def clear_folder():
+    folder_path = "result"
+
+    files = os.listdir(folder_path)
+
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                os.rmdir(file_path)
+        except Exception as e:
+            print(f"Error deleting {file_path}: {e}")
 
 
 if __name__ == "__main__":
+    print("License Plate Detection\n")
+    clear_folder()
     main()
+
+    print("End of Program.")
+    print()
--- modules/license_plate.py
+++ modules/license_plate.py
@@ -1,37 +1,44 @@
 import cv2
-import numpy as np
 import imutils
-import easyocr
-# this needs to run only once to load the model into memorys
-reader = easyocr.Reader(['en'])
 
 
 class LicensePlateDetector():
-    def __init__(self, image_path, auto_canny=0, binary=0, debug=False):
+    def __init__(self, image_path, auto_canny=0, debug=True):
+        '''
+        Initialization of object
+        '''
         self.image = cv2.imread(image_path)
         self.image = imutils.resize(self.image, height=500, width=500)
+        self.gray = None
 
         self.auto_canny = auto_canny
-        self.binary = binary
         self.debug = debug
 
     def debug_imshow(self, title, image):
+        '''
+        Save image for debugging process
+        '''
         if self.debug:
             debug_file_path = f"result/debug-{title}.jpg"
             cv2.imwrite(debug_file_path, image)
 
-    def find_plate(self, keep=30):
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+    def find_contour(self, keep=30):
+        '''
+        Find contours on image
+        '''
+        self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-        gray = cv2.bilateralFilter(gray, 3, 105, 105)
-        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        self.gray = cv2.bilateralFilter(self.gray, 3, 105, 105)
+        self.gray = cv2.GaussianBlur(self.gray, (5, 5), 0)
 
+        # edge detection using canny based on args
         if (self.auto_canny):
-            edge = imutils.auto_canny(gray)
+            edge = imutils.auto_canny(self.gray)
         else:
-            edge = cv2.Canny(gray, 170, 200)
+            edge = cv2.Canny(self.gray, 170, 200)
         self.debug_imshow("1_canny", edge)
 
+        # find contour based on edged image
         contours, _ = cv2.findContours(
             edge.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         temp_image = self.image.copy()
@@ -41,13 +48,19 @@ class LicensePlateDetector():
         sorted_contours = sorted(
             contours, key=cv2.contourArea, reverse=True)[:keep]
 
+        return sorted_contours
+
+    def find_plate_by_corner(self, contours):
+        '''
+        Find plate by finding 4 corner on contours input
+        '''
         plate_contour = None
         ok = 0
 
-        for c in sorted_contours:
-            contour_perimeter = cv2.arcLength(c, True)
+        for contour in contours:
+            contour_perimeter = cv2.arcLength(contour, True)
             epilson = 0.018 * contour_perimeter
-            approx = cv2.approxPolyDP(c, epilson, True)
+            approx = cv2.approxPolyDP(contour, epilson, True)
 
             if len(approx) == 4:
                 plate_contour = approx
@@ -55,51 +68,43 @@ class LicensePlateDetector():
                 break
 
         if not ok:
-            return ok, None, None
+            return ok, None
 
         temp_image = self.image.copy()
         cv2.drawContours(temp_image, [plate_contour], -1, (0, 255, 0), 2)
-        self.debug_imshow("3_license_contour", temp_image)
+        self.debug_imshow("3a_license_contour", temp_image)
 
-        x, y, w, h = cv2.boundingRect(plate_contour)
-        roi = gray[y:y+h, x:x+w]
+        return ok, plate_contour
 
-        return ok, roi, plate_contour
+    def find_plate_by_ratio(self, contours):
+        '''
+        Find plate by aspect ratio and highest area on contours input
+        '''
+        # find plate contour method 2
+        # using aspect ratio and contour area to find it
+        potential_plate_contours = []
+        ok = 0
 
-    def plate_ocr(self, roi):
-        if (self.binary):
-            _, binary_roi = cv2.threshold(
-                roi, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            aspect_ratio = w / float(h)
+            area = cv2.contourArea(contour)
 
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-            closed_roi = cv2.morphologyEx(binary_roi, cv2.MORPH_CLOSE, kernel)
+            if aspect_ratio > 2 and aspect_ratio < 4.5 and area > 1000:
+                potential_plate_contours.append(contour)
 
-            roi = cv2.morphologyEx(closed_roi, cv2.MORPH_OPEN, kernel)
+        if potential_plate_contours:
+            plate_contour = max(
+                potential_plate_contours, key=cv2.contourArea)
+            ok = 1
 
-        self.debug_imshow("4_final_roi", roi)
+        if not ok:
+            return ok, None
 
-        try:
-            result = reader.readtext(roi)
-            if result:  # Check if there's any result
-                license_plate_text = result[0][-2]
-                license_plate_text = ' '.join([elem[1] for elem in result])
-                ok = 1
-            else:
-                license_plate_text = "Plate not detected"
-                ok = 0
-        except Exception as e:
-            license_plate_text = "Error: " + str(e)
-            ok = 0
+        if plate_contour is not None:
+            temp_image = self.image.copy()
+            x, y, w, h = cv2.boundingRect(plate_contour)
+            cv2.rectangle(temp_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            self.debug_imshow("3b_license_contour", temp_image)
 
-        return ok, license_plate_text
-
-    def draw_result(self, plate_contour, license_plate_text):
-        final_img = self.image.copy()
-
-        x, y, w, h = cv2.boundingRect(plate_contour)
-        x1, y1, x2, y2 = x, y, x + w, y + h
-
-        cv2.rectangle(final_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(final_img, license_plate_text, (x1, y1 - 5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
-        cv2.imwrite("result/result.jpg", final_img)
+        return ok, plate_contour
